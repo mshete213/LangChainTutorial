466c25a1ca81d3abc0089a3552d6e848ea8a20b7
Maanas <ms2781@scarletmail.rutgers.edu>
2025-11-20 00:57:47 -0500

    with_strcutured_input

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 from langchain_openai import ChatOpenAI
@@ -10,43 +11,59 @@ from langchain_tavily import TavilySearch
 from prompt import rawReactFormatInstructions
 from schemas import AgentResponse
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
-from langchain_core.runnables import RunnableLambda
-#turns any python function -> usually a lambda function -> into a Runnable
-#Runnable is a class that is used to run the agent (invokable)
 
+# from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.runnables import RunnableLambda
+
+# turns any python function -> usually a lambda function -> into a Runnable
+# Runnable is a class that is used to run the agent (invokable)
 
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4", temperature=0)
-outputParser = PydanticOutputParser (pydantic_object=AgentResponse)
-reactPromptWithFormat = PromptTemplate(template=rawReactFormatInstructions, input_variables=["input", "agent_scratchpad", "tool_names"], output_parser=outputParser).partial(format_instructions=outputParser.get_format_instructions())
+structuredLLM = llm.with_structured_output(AgentResponse)
+# use with_structured_output to create a structured output LLM, accepts pydantic object as input
 
 
+# outputParser = PydanticOutputParser (pydantic_object=AgentResponse)
+# reactPromptWithFormat = PromptTemplate(template=rawReactFormatInstructions, 
+    # input_variables=["input", "agent_scratchpad", "tool_names"], 
+    # output_parser=outputParser).partial(format_instructions=
+    # outputParser.get_format_instructions())
+reactPromptWithFormat = PromptTemplate(
+    template=rawReactFormatInstructions,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions="")
 
-#this is the special react prompt, more on this later
+
+# this is the special react prompt, more on this later
 react_prompt = hub.pull("hwchase17/react")
 
-#This creates the agent, which is a react agent that uses the tools to search the web and return the results
-#This is the actual runtime object, which is like the "body" to the agents "brain"
-#I need to pass tools to both
-agent = create_react_agent(llm = llm, tools = tools, prompt = reactPromptWithFormat)
+# This creates the agent, which is a react agent that uses the tools to search the web and return the results
+# This is the actual runtime object, which is like the "body" to the agents "brain"
+# I need to pass tools to both
+agent = create_react_agent(llm=llm, tools=tools, prompt=reactPromptWithFormat)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-#outputs dictionary with keys "output" and "intermediate steps"
+# outputs dictionary with keys "output" and "intermediate steps"
 extractOutput = RunnableLambda(lambda x: x["output"])
-#extracts the output from the dictionary into a JSON string ("{:}")
-parseOutput = RunnableLambda(lambda x: outputParser.parse(x))
-#converts the JSON string into a pydantic object, will have structure set in schemas.py along with other metadata
-#that class inherits BaseModel
+# extracts the output from the dictionary into a JSON string ("{:}")
+# parseOutput = RunnableLambda(lambda x: outputParser.parse(x))
+# converts the JSON string into a pydantic object, will have structure set in schemas.py along with other metadata
+# that class inherits BaseModel
 
-chain = agent_executor | extractOutput | parseOutput
+chain = agent_executor | extractOutput | structuredLLM
 
 
 def main():
     print("Hello from langchaintutorial!")
 
-    result = chain.invoke({"input": "search for 3 job openings for an ai engineer using langchain in the bay area on linkedin and list their details."})
+    result = chain.invoke(
+        {
+            "input": "search for 3 job openings for an ai engineer using langchain in the bay area on linkedin and list their details."
+        }
+    )
     print(result)
+
 
 if __name__ == "__main__":
     main()
--- schemas.py
+++ schemas.py
@@ -1,11 +1,12 @@
-#pydantic is a library for data validation and typing
-#pydantic is automatically installed with langchain
+# pydantic is a library for data validation and typing
+# pydantic is automatically installed with langchain
 
 from typing import List
 
-#base model is the base class for all pydantic models
-#Field is a class that is used to define the fields of the model, default values, descriptions, other metadata
+# base model is the base class for all pydantic models
+# Field is a class that is used to define the fields of the model, default values, descriptions, other metadata
 from pydantic import BaseModel, Field
+
 
 class Source(BaseModel):
     """Schema for a source used by the agent"""
@@ -15,8 +16,10 @@ class Source(BaseModel):
 
 class AgentResponse(BaseModel):
     """Schema for the response of the agent"""
-    #source class was defined above, so we can use it here
+
+    # source class was defined above, so we can use it here
     answer: str = Field(description="The agent's answer to the question")
-    sources: List[Source] = Field(default_factory=list, description="The list of sources used to answer the question")
-
-
+    sources: List[Source] = Field(
+        default_factory=list,
+        description="The list of sources used to answer the question",
+    )
